6d48ea4517bd6447b6d1c69522e83c644b0022e5
Aren <arenkdesai@gmail.com>
2026-01-05 20:07:48 -0600

    Adding chronos test

---
+++ src/prediction.py
@@ -0,0 +1,19 @@
+import pandas as pd  # requires: pip install 'pandas[pyarrow]'
+from chronos import Chronos2Pipeline
+import polars as pl
+
+all_prices = pl.read_csv("data.csv").drop(["mlc", "mcc"])
+pipeline = Chronos2Pipeline.from_pretrained("amazon/chronos-2", device_map="cuda")
+
+# Load historical target values and past values of covariates
+context_df = all_prices.to_pandas()
+
+# Generate predictions with covariates
+pred_df = pipeline.predict_df(
+    context_df,
+    prediction_length=12,  # Number of steps to forecast
+    quantile_levels=[0.1, 0.5, 0.9],  # Quantile for probabilistic forecast
+    id_column="location",  # Column identifying different time series
+    timestamp_column="datetime",  # Column with datetime information
+    target="lmp",  # Column(s) with time series values to predict
+)
